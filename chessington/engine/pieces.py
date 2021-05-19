b6b966e02b795cdf012cb714234e970074238fbb
WJC <wj.colley@hotmail.co.uk>
2021-05-19 15:14:07 +0100

    Pawns now allowed to capture diagonally

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -38,23 +38,57 @@ class Pawn(Piece):
         current_square = board.find_piece(self)
         moves = []
         if self.player == Player.WHITE:
-            if current_square.row < 7:
+            if current_square.row < 7: # protecting run off end of board
+
+                # legal forward move
                 candidate_move = Square.at((current_square.row)+1,current_square.col)
                 if board.get_piece(candidate_move) is None:
                     moves.append(candidate_move)
+                
+                # legal initial move
                 if current_square.row == 1:
                     special_case_candidate_move = Square.at((current_square.row)+2,current_square.col)
                     if board.get_piece(special_case_candidate_move) is None and board.get_piece(candidate_move) is None:
                         moves.append(special_case_candidate_move)
                 
-        elif current_square.row > 0:
+                # diagonal taking enemies
+                if current_square.col > 0:
+                    first_diagonal = Square.at(current_square.row+1,current_square.col-1)
+                    possible_piece = board.get_piece(first_diagonal)
+                    if possible_piece is not None and possible_piece.player == Player.BLACK:
+                        moves.append(first_diagonal)
+                if current_square.col < 7:
+                    second_diagonal = Square.at(current_square.row+1,current_square.col+1)
+                    possible_piece = board.get_piece(second_diagonal)
+                    if possible_piece is not None and possible_piece.player == Player.BLACK:
+                        moves.append(second_diagonal)
+
+                
+        elif current_square.row > 0: # protecting run off end of board
+
+            # legal forward move
             candidate_move = Square.at((current_square.row)-1,current_square.col)
             if board.get_piece(candidate_move) is None:
                 moves.append(candidate_move)
+            
+            # legal initial move
             if current_square.row == 6:
                 special_case_candidate_move = Square.at((current_square.row)-2,current_square.col)
                 if board.get_piece(special_case_candidate_move) is None and board.get_piece(candidate_move) is None:
-                    moves.append(special_case_candidate_move)         
+                    moves.append(special_case_candidate_move)
+            
+            # diagonal taking enemies
+            if current_square.col > 0:
+                first_diagonal = Square.at(current_square.row-1,current_square.col-1)
+                possible_piece = board.get_piece(first_diagonal)
+                if possible_piece is not None and possible_piece.player == Player.WHITE:
+                    moves.append(first_diagonal)
+            if current_square.col < 7:
+                second_diagonal = Square.at(current_square.row-1,current_square.col+1)
+                possible_piece = board.get_piece(second_diagonal)
+                if possible_piece is not None and possible_piece.player == Player.WHITE:
+                    moves.append(second_diagonal)
+                   
         return moves
 
     def square_occupied(candidate_move,moves,board):
